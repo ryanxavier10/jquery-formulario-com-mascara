54292ea8e2edb534e26cae9032524de1df90ac90
Ryan Xavier <lugialga@gmail.com>
2024-02-09 12:11:31 -0300

    commit do projeto

---
+++ Js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('#CPF').mask('000.000.000-00');
+
+    $('#CEP').mask('000000-000');
+
+    $('form').validate({
+        rules:{
+            nome: {
+                required: true
+        }, 
+        email: {
+            required: true,
+            email:true
+        }, 
+        telefone: {
+            required: true,
+        },
+        CPF: {
+            required: true,
+        },
+        endereço: {
+            required: true,
+        },
+        CEP: {
+            required: true,
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome'
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    }
+})
+});
